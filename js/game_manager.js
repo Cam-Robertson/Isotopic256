55f1fdfea0f705209a1671c41a7419bd526aec32
Cam-Robertson <44587090+Cam-Robertson@users.noreply.github.com>
2020-04-15 09:59:58 -0400

    Get rid of Tile disappear

--- js/game_manager.js
+++ js/game_manager.js
@@ -135,10 +135,7 @@ GameManager.prototype.move = function (direction) {
               var merged = new Tile(positions.next, tile.value * 2);
               merged.mergedFrom = [tile, next];
 
-              merged.unstable = merged.value === 8
-                             || merged.value === 32
-                             || merged.value === 128
-                             || merged.value > 256 ? (merged.value <= 256 ? merged.value - (merged.value / 4) : 81) : 0;
+             
 
               // Update the score
               self.score += merged.value + (tile.unstable > 0 ? tile.unstable * 2 : 0) + (next.unstable > 0 ? next.unstable * 2 : 0);
